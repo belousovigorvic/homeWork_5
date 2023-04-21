b3a4d388800c3277e512c7f8828654e147ebe8c1
belousovigorvic <68902284+belousovigorvic@users.noreply.github.com>
2023-04-21 17:53:33 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 // Reverse Words Function
-function reverseWords(...words) { // создаем функцию reverseWords c параметром ...words (...не ограничное кол-во значений)
-    return words.map(word => { // обращаемся к параметру words, проходимся по всему массиву методом map
-        let reversedWord = ''; // создаем пустую строку
-        for (let i = word.length; i >= 0; i--) { // создаем "обратный" цикл, который проходится по всей длине каждого слова, с конца в начало.
-            reversedWord += word.charAt(i); // применяем метод строки charAt в которую передаем итератор i сохраняем в переменную reversedWord
+function reverseWords(...words) {
+    return words.map(word => {
+        let reversedWord = '';
+        for (let i = 0; i < word.length; i++) {
+            reversedWord = word[i] + reversedWord;
         }
-        return reversedWord; // возращаем результат
+        return reversedWord;
     });
 }
-console.log(reverseWords('фронтенд', 'апрель', 'урок', 'Игорь', 'автомобиль')); // выводим результат
+
+console.log(reverseWords('Hello', 'Igor', 'Julia', 'Bishkek'))
 
 // Average Function
-const getAverageNumber = function (...numbers) { // Создаем константу в которую записана функция с параметром ...numbers (...не ограничное кол-во значений)
-    const sum = numbers.reduce((acc, curr) => acc + curr) // Создаем переменную в которой обращаемся к параметру numbers и применяем метод reduce, получаем сумму всех чисел
-    const average= sum / numbers.length // сумму делим на количество чисел в массиве
-    console.log(average) // выводим результат
+const getAverageNumber = function (...numbers) { 
+    const sum = numbers.reduce((acc, curr) => acc + curr) 
+    const average= sum / numbers.length 
+    console.log(average) 
 }
-getAverageNumber(1,2,3,4,5,6,7,85) // вызываем функцию и передаем аргумент
+getAverageNumber(1,2,3,4,5,6,7,85) 
 
 // getCoffee Function
 function getCoffee() { // Создаем функцию getCoffee
-    let drinkName = prompt('What drink would you like? (coffee)').toLowerCase() // Спрашивем будет ли клиент кофе? и ловим ошибку методом toLowerCase
-    let coffee = drinkName === 'coffee' ? prompt('What do you want your coffee? (black/with cream)').toLowerCase() : null // Записываем в переменную логику тернарного оператора
+    let drinkName = prompt('What drink would you like? (coffee)').toLowerCase() 
+    let coffee = drinkName === 'coffee' ? prompt('What do you want your coffee? (black/with cream)').toLowerCase() : null 
     coffee === 'black' ? alert('You have chosen black coffee.') :
         coffee === 'with cream' ? alert('You have chosen coffee with cream.') :
             alert('You did not choose a valid coffee option.')
 }
-getCoffee() // вызываем функцию
+getCoffee() 
